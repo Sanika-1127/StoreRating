5a3a537897bf6ae4cf1b6f232c1b4f3ae869c501
Sanika Godase <sanikagodase2004@gmail.com>
2025-11-10 20:40:43 +0530

    Update Login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/index";
-import Login from "./pages/login";
+import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import AdminDashboard from "./pages/AdminDashboard";
 import UserDashboard from "./pages/UserDashboard";
